fe5e768b32f5275ae200414d19fef42f17ae3a5d
Aalivexy <aalivexy@outlook.com>
2026-07-03 20:40:06 +0800

    use weak for __vcrt_* functions

---
+++ asm/weak.asm
@@ -0,0 +1,5 @@
+ALIAS <__vcrt_GetModuleFileNameW> = <GetModuleFileNameW>
+ALIAS <__vcrt_GetModuleHandleW> = <GetModuleHandleW>
+ALIAS <__vcrt_LoadLibraryExW> = <LoadLibraryExW>
+ALIAS <__vcrt_InitializeCriticalSectionEx> = <InitializeCriticalSectionEx>
+END
--- build.rs
+++ build.rs
@@ -13,32 +13,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         _ => return Ok(()),
     };
 
+    let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
     let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
-    let def_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("def");
 
     let lib = find_msvc_tools::find_tool(arch, "lib.exe")
         .map(|t| t.path().to_path_buf())
-        .expect("lib.exe not found; ensure Visual Studio or MSVC build tools are installed");
-
-    for (name, def) in [
-        ("vcruntime", def_dir.join(format!("vcruntime_{arch}.def"))),
-        ("ucrt", def_dir.join(format!("ucrt_{arch}.def"))),
-    ]
-    .into_iter()
-    .chain((arch != "x86").then(|| ("vcruntime.weak", def_dir.join("vcruntime.weak.def"))))
-    {
+        .expect("lib.exe not found");
+    for name in ["vcruntime", "ucrt"] {
+        let def = root.join("def").join(format!("{name}_{arch}.def"));
         let output = out_dir.join(format!("{name}.lib"));
         let status = Command::new(&lib)
-            .args([
-                &format!("/def:{}", def.display()),
-                &format!("/out:{}", output.display()),
-                &format!("/machine:{arch}"),
-            ])
+            .arg(format!("/def:{}", def.display()))
+            .arg(format!("/out:{}", output.display()))
+            .arg(format!("/machine:{arch}"))
             .status()?;
         if !status.success() {
             return Err(format!("lib.exe exited with {status}").into());
         }
         println!("cargo:rerun-if-changed={}", def.display());
+    }
+
+    if arch != "x86" {
+        let asm = root.join("asm").join("weak.asm");
+        let obj = out_dir.join("weak.obj");
+
+        let masm_exe = match arch {
+            "x64" => "ml64.exe",
+            _ => "armasm64.exe",
+        };
+        let masm = find_msvc_tools::find_tool(arch, masm_exe)
+            .map(|t| t.path().to_path_buf())
+            .expect("masm not found");
+
+        let status = Command::new(&masm)
+            .arg("/c")
+            .arg(format!("/Fo{}", obj.display()))
+            .arg(&asm)
+            .status()?;
+        if !status.success() {
+            return Err(format!("masm exited with {status}").into());
+        }
+        let status = Command::new(&lib)
+            .arg(&out_dir.join("vcruntime.lib"))
+            .arg(&obj)
+            .status()?;
+        if !status.success() {
+            return Err(format!("lib.exe exited with {status}").into());
+        }
+        println!("cargo:rerun-if-changed={}", asm.display());
     }
 
     println!("cargo:rustc-link-search={}", out_dir.display());
